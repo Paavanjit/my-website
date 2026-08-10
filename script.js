182585f30fa06841be4b055090b5f18a789f86b3
Paavanjit <paavanjitsingh4597@gmail.com>
2026-08-10 22:18:12 +0530

    Add click message return function

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ function scrollToSection(sectionId) {
   document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
 
+function getClickMessage(count) {
+  return 'Learn More clicked ' + count + ' times';
+}
+
 document.getElementById('learnMoreBtn').addEventListener('click', function (e) {
   e.preventDefault();
   scrollToSection('about');
 
   clickCount = clickCount + 1;
-  document.getElementById('clickCount').textContent = 'Learn More clicked ' + clickCount + ' times';
+  document.getElementById('clickCount').textContent = getClickMessage(clickCount);
 });
 
 document.getElementById('aboutNavLink').addEventListener('click', function (e) {
