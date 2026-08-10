c401c17e1c30646a42326d866ed59f0c7199a691
Paavanjit <paavanjitsingh4597@gmail.com>
2026-08-10 21:32:18 +0530

    Add click event navigation

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('learnMoreBtn').addEventListener('click', function (e) {
   e.preventDefault();
   document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
 });
+ 
+document.getElementById('aboutNavLink').addEventListener('click', function (e) {
+  e.preventDefault();
+  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
+ 
